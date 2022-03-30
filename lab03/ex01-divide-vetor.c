e417397c6e0023a4564398e57101223bccb70e37
Joao Moraski <joaomoraskilunkes@gmail.com>
2022-03-30 10:51:23 -0300

    adicionando comentarios e descricao ao codigo

--- lab03/ex01-divide-vetor.c
+++ lab03/ex01-divide-vetor.c
@@ -1,6 +1,9 @@
-//
-// Created by moraski on 29/03/2022.
-//
+/*
+ * Created by moraski on 29/03/2022.
+ * O código abaixo recebe como parâmetros o número de threads, o valor a procurar e o tamanho do vetor,
+ * se executado sem os parâmetros ele da erro e mostra os parâmetros necessários.
+ * A ideia do código é separar o vetor em N threads e buscar um valor dentro destes vetores com cada thread buscando para sua parte do vetor.
+ * */
 #include "stdio.h"
 #include "stdlib.h"
 #include "pthread.h"
@@ -33,24 +36,28 @@ void *createThread(void *param) {
     struct SearchInfo *thread = param;
     for (int i = thread->initialPosition; i < thread->finalPosition; ++i) {
         if (thread->vector[i] == thread->key) {
-            // +1 para mostrar certo no log
+            // +1 para mostrar certo a thread no log
             printf("A %d° thread encontrou o valor %d no vetor.\n", thread->threadPosition + 1, thread->key);
         }
     }
 }
 
 int main(int argc, char **argv) {
+    // Ifs de garantia para os parametros estarem certos
     if (argc > 4) return printf("Quantidade de parametros errada");
-    if (argc < 4) {
-        printf("Informe o numero de threads e o valor a procurar na execução do programa e o tamanho do vetor\n");
-        return 0;
-    }
+    if (argc < 4) return printf("Informe o numero de threads e o valor a procurar na execução do programa e o tamanho do vetor\n");
+
+    // Variaveis tiradas dos args para facilitar futuramente
     int nThreads = atoi(argv[1]), search = atoi(argv[2]), vetLen = atoi(argv[3]), init, final;
+    // 8 threads de limite (Os testes deste algortimo foram feitos em um computador com um processamento mais baixo)
     if (nThreads > 8) return 0;
 
     pthread_t vecThreads[nThreads];
     int *vector = random_vector(vetLen, 10, 42);
 
+    /*
+     * Calculo do inicio e do fim do vetor para a divisao ficar correta
+     * */
     for (int i = 0; i < nThreads; ++i) {
         init = i * (vetLen / nThreads);
         if (i + 1 > nThreads) final = vetLen;
--- lab03/ex02-frequencia-elemento.c
+++ lab03/ex02-frequencia-elemento.c
@@ -1,25 +1,26 @@
-//
-// Created by moraski on 29/03/2022.
-//
+/*
+ * Created by moraski on 29/03/2022.
+ * A ideia do código é separar o vetor em 2 threads e calcular a frequencia de valores no vetor, e apos isso printar todos.
+ * */
 #include "stdio.h"
 #include "stdlib.h"
 #include "pthread.h"
 
-typedef struct SearchInfo {
+typedef struct ThreadInfo {
     int initialPosition, finalPosition, threadPosition, *vector;
-} SearchInfo;
+} ThreadInfo;
 
 // Usando uma ideia baseada na orientacao objeto e no exercicio 03 de exemplos, seria mais facil instanciar structs das threads do que criar uma por uma manualmente
-SearchInfo *createSearch(int initialPosition, int finalPosition, int threadPosition, int *vector) {
-    SearchInfo *searchInfo = (SearchInfo *) malloc(sizeof(SearchInfo));
-    searchInfo->initialPosition = initialPosition;
-    searchInfo->finalPosition = finalPosition;
-    searchInfo->vector = vector;
-    searchInfo->threadPosition = threadPosition;
-    return searchInfo;
+ThreadInfo *createThreadStruct(int initialPosition, int finalPosition, int threadPosition, int *vector) {
+    ThreadInfo *threadInfo = (ThreadInfo *) malloc(sizeof(ThreadInfo));
+    threadInfo->initialPosition = initialPosition;
+    threadInfo->finalPosition = finalPosition;
+    threadInfo->vector = vector;
+    threadInfo->threadPosition = threadPosition;
+    return threadInfo;
 };
 
-int numberFreq[2][10];
+int numberFreq[2][10]; // variavel global para salvar a frequencia dos numeros
 
 int *random_vector(int n, int max, int seed) {
     int *vector = malloc(sizeof(int) * n);
@@ -31,9 +32,9 @@ int *random_vector(int n, int max, int seed) {
 }
 
 void *createThread(void *param) {
-    struct SearchInfo *thread = param;
-    for (int i = thread->initialPosition; i < thread->finalPosition; ++i) {
-        numberFreq[thread->threadPosition][thread->vector[i]]++;
+    struct ThreadInfo *threadInfo = param;
+    for (int i = threadInfo->initialPosition; i < threadInfo->finalPosition; ++i) {
+        numberFreq[threadInfo->threadPosition][threadInfo->vector[i]]++;
     }
 }
 
@@ -41,20 +42,28 @@ int main() {
     pthread_t vecThreads[2];
     int *vector = random_vector(1000, 10, 42), init, final;
     for (int i = 0; i < 2; ++i) {
+
+        /*
+         * Calculo do inicio e do fim do vetor para a divisao ficar correta
+         * */
         init = i * (1000 / 2);
         if (i + 1 > 2) final = 1000;
         else final = ((1000 / 2) - 1) * (i + 1);
 
-        SearchInfo *searchInfo = createSearch(init, final, i, vector);
+        // Criacao da struct da thread
+        ThreadInfo *threadInfo = createThreadStruct(init, final, i, vector);
 
-        pthread_create(&vecThreads[i], NULL, createThread, (void *) searchInfo);
+        pthread_create(&vecThreads[i], NULL, createThread, (void *) threadInfo);
     }
 
+    // Espera as duas terminar para que seja feito os calculos
     pthread_join(vecThreads[0], NULL);
     pthread_join(vecThreads[1], NULL);
-    // Espera as duas terminar para que seja feito os calculos
+
+    // Print final das frequencia somadas
     printf("Frequencia no vetor de 1000 números: \n");
     printf("\t (Numero - Frequencia)\n");
+    // Codigo esta fixo de i < 10 pq é pedido de 0 a 9 no exercicio
     for (int i = 0; i < 10; ++i) {
         printf("\t (%d - %d)\n", i, numberFreq[0][i] + numberFreq[1][i]);
     }
