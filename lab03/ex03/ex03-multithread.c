7294ac8402b91ffa0a537af149de0708799b705c
João Moraski <joaomoraskilunkes@gmail.com>
2022-03-30 22:37:07 -0300

    ta tudo errado

--- lab03/ex03/ex03-multithread.c
+++ lab03/ex03/ex03-multithread.c
@@ -10,7 +10,7 @@
 #include "matriz/matriz.h"
 #include "sorts/quicksort.h"
 
-#define SIZE 20
+#define SIZE 1000
 
 typedef struct ThreadInfo {
     int initialPosition, finalPosition, threadPosition, **matriz;
@@ -31,12 +31,24 @@ int mediaAritmeticaVec[SIZE];
 
 void *mediana(void *param) {
     struct ThreadInfo *threadInfo = param;
-    for (int i = threadInfo->initialPosition; i < threadInfo->finalPosition; ++i) {
+    for (int i = threadInfo->initialPosition; i <= threadInfo->finalPosition; ++i) {
+        printf("inicio: %d\nfim: %d\ni: %d\n", threadInfo->initialPosition, threadInfo->finalPosition, i);
+        printf("\n\ncore dumpoud\n\n");
+        printf("\n%d\n", i);
+        if (i == 383){
+            for (int j = 0; j < SIZE; ++j) {
+                printf("\n%d\n", threadInfo->matriz[382][j]);
+            }
+        }
         QuickSort(threadInfo->matriz[i], threadInfo->matriz[i][0], threadInfo->matriz[i][SIZE]);
-        if (SIZE / 2 == 0) {
-            medianaVec[i] = threadInfo->matriz[i][SIZE/2] + threadInfo->matriz[i][(SIZE/2)+1];
-        } else{
-            medianaVec[i] = threadInfo->matriz[i][SIZE/2];
+        printf("\n\ncore dumpoua\n\n");
+        if (SIZE % 2 == 0) {
+            printf("\n\ncore dumpou\n\n");
+            medianaVec[i] = threadInfo->matriz[i][SIZE / 2] + threadInfo->matriz[i][(SIZE / 2) + 1];
+            printf("\n\ncore dumpoub\n\n");
+        } else {
+            medianaVec[i] = threadInfo->matriz[i][SIZE / 2];
+            printf("\n\ncore dumpouc\n\n");
         }
     }
 }
@@ -47,7 +59,7 @@ void *mediaAritmetica(void *param) {
         int soma = 0;
         for (int j = 0; j < SIZE; ++j) {
             soma += threadInfo->matriz[i][j];
-            mediaAritmeticaVec[i] = soma/SIZE;
+            mediaAritmeticaVec[i] = soma / SIZE;
         }
     }
 }
@@ -69,37 +81,39 @@ int main(int argc, char **argv) {
         else final = ((SIZE / nThreads) - 1) * (i + 1);
 
         // Criacao da struct da thread
+//        printf("i: %d\nchamou a thread\ninit: %d\nfinal: %d\n", i, init, final);
         ThreadInfo *threadInfo = createThreadStruct(init, final, i, matriz);
 
         pthread_create(&vecThreadsA[i], NULL, mediana, (void *) threadInfo);
-    }
-
-    for (int i = 0; i < nThreads; ++i) {
-        /*
-         * Calculo do inicio e do fim do vetor para a divisao ficar correta
-         * */
-        init = i * (SIZE / nThreads);
-        if (i + 1 > nThreads) final = SIZE;
-        else final = ((SIZE / nThreads) - 1) * (i + 1);
-
-        // Criacao da struct da thread
-        ThreadInfo *threadInfo = createThreadStruct(init, final, i, matriz);
-
-        pthread_create(&vecThreadsB[i], NULL, mediaAritmetica, (void *) threadInfo);
-    }
-
-    for (int i = 0; i < nThreads; ++i) {
         pthread_join(vecThreadsA[i], NULL);
-        pthread_join(vecThreadsB[i], NULL);
     }
 
-    for (int i = 0; i < SIZE; ++i) {
+//    for (int i = 0; i < nThreads; ++i) {
+//        /*
+//         * Calculo do inicio e do fim do vetor para a divisao ficar correta
+//         * */
+//        init = i * (SIZE / nThreads);
+//        if (i + 1 > nThreads) final = SIZE;
+//        else final = ((SIZE / nThreads) - 1) * (i + 1);
+//
+//        // Criacao da struct da thread
+//        ThreadInfo *threadInfo = createThreadStruct(init, final, i, matriz);
+//
+//        pthread_create(&vecThreadsB[i], NULL, mediaAritmetica, (void *) threadInfo);
+//    }
+
+//    for (int i = 0; i < nThreads; ++i) {
+//        pthread_join(vecThreadsA[i], NULL);
+//        pthread_join(vecThreadsB[i], NULL);
+//    }
+
+    for (int i = 0; i < 10; ++i) {
         printf("Mediana: %d\n", medianaVec[i]);
     }
 
-    for (int i = 0; i < SIZE; ++i) {
-        printf("media: %d\n", mediaAritmeticaVec[i]);
-    }
+//    for (int i = 0; i < SIZE; ++i) {
+//        printf("media: %d\n", mediaAritmeticaVec[i]);
+//    }
 
 
     return 0;
